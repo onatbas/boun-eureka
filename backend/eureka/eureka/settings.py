73efea360820a532782cd08bbb866cc30223fc6d
Onat Bas <git@onat.me>
2017-10-04 17:35:18 +0300

    (update) Dockerfile settings and python set to 3.6
           Reasons
    =====================
    1. When running from docker container, we have to explicitly state the
    port to run in.
    2. Python version 3.6 is more suited for the team.
    3. Django version 1.11.5 is most recent.
    
           Changes
    =====================
    1. Upgraded django and python versions.
    2. Dockerfile is set to explicitly run on 8000

--- backend/eureka/eureka/settings.py
+++ backend/eureka/eureka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=@sq0*-pma5g)9%ke_atvcz-af2zvwb4e6k&+d3rxprfr-j(++'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
