99339adc70fa60c1c1c07e5d04a655b3cb0f9657
Onat Bas <git@onat.me>
2017-10-14 11:03:42 +0300

    (add) token based authentication implemented
           Reasons
    =====================
    1. We will be using token based authentication.
    2. The user will have the API to interact with the product based on the
    token they provide if they wish to access protected resources.
    3. These tokens shall be provided by the api call.
    
           Changes
    =====================
    1. Token based authentication implemented.
    2. AuthMiddleware implemented in the source code to chek for token
    validity.
    3. a sample test method added to showcase the auth process on a live
    endpoint.
    4. Tokens model added, necessary migration scripts are included.
    5. middleware, validator, dto packages added

--- backend/eureka/api/urls.py
+++ backend/eureka/api/urls.py
@@ -7,6 +7,9 @@ app_name = 'api'
 urlpatterns = [
     url(r'^user/login/$', api_login, name='api_login'),
     url(r'^user/register/$', api_register, name='api_register'),
+    url(r'^test/$', test, name='api_test'),
 ]
+
+
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/eureka/eureka/settings.py
+++ backend/eureka/eureka/settings.py
@@ -65,7 +65,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+
+
 
 ROOT_URLCONF = 'eureka.urls'
 
