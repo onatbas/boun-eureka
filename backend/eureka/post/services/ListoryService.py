d7bc03584f2c414606dc7995041c2873bc660840
Onat Bas <git@onat.me>
2017-10-21 11:40:07 +0300

    (add) wrapped api service and view in try-catch
           Reasons
    =====================
    1. Currently, the exception if happens breaks the communication.
    2. We need to communicate with the consumer with the correct HTTP code
    and response.
    
           Changes
    =====================
    1. try-except blocks implemented, which returns appropriate http
    statuses

--- backend/eureka/post/services/ListoryService.py
+++ backend/eureka/post/services/ListoryService.py
@@ -2,10 +2,13 @@ from post.models import Post
 
 
 def create_listory(name, description, owner):
+    try:
         listory = Post.objects.create(user=owner, content=description, title=name)
 
         listory.save()
         return listory.pk
+    except:
+        return ""
 
 
 def get_listory_by_id(listoryId):
