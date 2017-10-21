28e03383d2ca9b05a014ee800cc6dd671a3e0161
Onat Bas <git@onat.me>
2017-10-21 12:34:12 +0300

    (add) GET: /listory/ - get all listory endpoint
           Reasons
    =====================
    1. The implementation of /listory/ is needed. Docuemntation was given in
    the previous commit.
    
           Changes
    =====================
    1. Implementation of retrieving all listories added.

--- backend/eureka/api/urls.py
+++ backend/eureka/api/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^test/$', test, name='api_test'),
     url(r'^listory/create/$', api_create_listory, name='api_create_listory'),
     url(r'^listory/(?P<id>\w{0,50})/$', api_listory, name='api_listory'),
+    url(r'^listory/$', get_all_listories, name='get_all_listories'),
 ]
 
 
