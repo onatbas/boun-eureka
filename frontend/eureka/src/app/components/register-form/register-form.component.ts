7d3c72a910ff90debbceab10a066c05ac98feb25
Muh <muharrem.unverr@gmail.com>
2017-10-31 11:33:48 +0300

    font awesome icons are created
    font awesome package is installed

--- frontend/eureka/src/app/components/register-form/register-form.component.ts
+++ frontend/eureka/src/app/components/register-form/register-form.component.ts
@@ -33,13 +33,14 @@ export class RegisterFormComponent {
 
     if (this.password != this.password2){
       this.message = "Passwords don't match";       
-    }else if (!this.password || this.password.length <= 8){
+    }else if (!this.password || this.password.length < 8){
       this.message = "Minimum password length is 8";   
-    }else if (!this.email){
+    }else if (this.email.length == 0){
       this.message = "Please type a valid email address";
+    }else if (this.username.length == 0){
+      this.message = "Please type a valid username";
     }else{
-      this.message = "";
-      
+      this.message = "";      
       this.userService.register(this.email, this.password, this.username);
     }
   }
