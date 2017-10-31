94dfcd646a02fcd97a18cd135a1280df21d454fa
Onat Bas <git@onat.me>
2017-10-31 13:57:56 +0300

    (add) Style updates for detail view
           Reasons
    =====================
    1. Inofmration such as category, creation date etc. are missing in the
    view.
    
           Changes
    =====================
    1. HTML filled to match mocukps

--- frontend/eureka/src/app/components/detail/detail.component.ts
+++ frontend/eureka/src/app/components/detail/detail.component.ts
@@ -21,7 +21,8 @@ constructor(
   private listoryService: ListoryService
 ) {}
 
-private listory: Listory = {
+private listory: Listory;
+/*{
     name: "Modadaki Tombili Heykeli",
     description:"Yaygın inancın tersine, Lorem Ipsum rastgele sözcüklerden oluşmaz. Kökleri M.Ö. 45 tarihinden bu yana klasik Latin edebiyatına kadar uzanan 2000 yıllık bir geçmişi vardır. Virginia'daki Hampden-Sydney College'dan Latince profesörü Richard McClintock, bir Lorem Ipsum pasajında geçen ve anlaşılması en güç sözcüklerden biri olan 'consectetur' sözcüğünün klasik edebiyattaki örneklerini incelediğinde kesin bir kaynağa ulaşmıştır. Lorm Ipsum, Çiçero tarafından M.Ö. 45 tarihinde kaleme alınan \"de Finibus Bonorum et Malorum\" (İyi ve Kötünün Uç Sınırları) eserinin 1.10.32 ve 1.10.33 sayılı bölümlerinden gelmektedir. Bu kitap, ahlak kuramı üzerine bir tezdir ve Rönesans döneminde çok popüler olmuştur. Lorem Ipsum pasajının ilk satırı olan \"Lorem ipsum dolor sit amet\" 1.10.32 sayılı bölümdeki bir satırdan gelmektedir. 1500'lerden beri kullanılmakta olan standard Lorem Ipsum metinleri ilgilenenler için yeniden üretilmiştir. Çiçero tarafından yazılan 1.10.32 ve 1.10.33 bölümleri de 1914 H. Rackham çevirisinden alınan İngilizce sürümleri eşliğinde özgün biçiminden yeniden üretilmiştir.",
     image:"http://www.kurandakidin.com/wp-content/uploads/2016/08/Heykel.jpg",
@@ -32,19 +33,19 @@ private listory: Listory = {
         name:"Onat Bas",
         userId:"4"
     },
-    createdAt:null,
+    createdAt:"8 minutes ago",
     time:{
         name:"Years",
         units:"YEAR",
-        values:[2015, 0]
+        values:[2015, 2017]
     }
    
-};
+};*/
 
   ngOnInit(): void {
- //   this.route.paramMap
- //     .switchMap((params: ParamMap) => this.listoryService.getListory(+params.get('id')))
- //     .subscribe(listory => this.listory = listory);
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.listoryService.getListory(+params.get('id')))
+      .subscribe(listory => this.listory = listory);
   }
 
 
