66c1ddc6d4211408cbfb68e426c590904cdce8ad
Onat Bas <git@onat.me>
2017-10-31 16:10:33 +0300

    Merge conflicts resolved

--- frontend/eureka/src/app/components/welcome-screen/welcome-screen.component.ts
+++ frontend/eureka/src/app/components/welcome-screen/welcome-screen.component.ts
@@ -3,10 +3,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../../services/user'
 import { WelcomeForm } from './form'
 import { UserService } from '../../services/user.service'
-<<<<<<< Updated upstream
-import { Subscription } from 'rxjs/Subscription';
-=======
->>>>>>> Stashed changes
 
 @Component({
   selector: 'welcome-screen',
@@ -17,13 +13,8 @@ export class WelcomeScreenComponent implements OnInit {
   
   @Input() user: User;
 
-<<<<<<< Updated upstream
   private form: WelcomeForm = new WelcomeForm(); // false means Login
-=======
-  private message = 'Welcome to Eureka!';
-
   onRegister: Boolean = true; // false means login.
->>>>>>> Stashed changes
 
   constructor(
     private userService: UserService,
@@ -32,17 +23,8 @@ export class WelcomeScreenComponent implements OnInit {
   ngOnInit(): void {
     this.userService.getUser().then(user => {
       this.user = user;
-<<<<<<< Updated upstream
     });  
-
   }
-
-
-=======
-    });
-  }
-
->>>>>>> Stashed changes
 }
 
 
