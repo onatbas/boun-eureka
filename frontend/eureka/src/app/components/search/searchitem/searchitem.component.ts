5fe61efea8b7197286e3f60b319cd104d0f1e581
Onat Bas <git@onat.me>
2017-12-06 17:16:27 +0300

    (add) Redirection to web page on search click
           Reasons
    =====================
    1. Page needs to redirect to listory detail page on click
    
           Changes
    =====================
    1. Added this functionality.

--- frontend/eureka/src/app/components/search/searchitem/searchitem.component.ts
+++ frontend/eureka/src/app/components/search/searchitem/searchitem.component.ts
@@ -1,14 +1,21 @@
 import { Component } from "@angular/core";
 import { Input } from "@angular/core";
 import { Listory } from "../../../services/Listory";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "searchitem",
     styleUrls: ["searchitem.component.css"],
     templateUrl: "searchitem.component.html"
 })
-export class SearchItemComponent{
+export class SearchItemComponent {
+    constructor(private router: Router) { }
+
 
     @Input() listory: Listory;
 
+
+    gotoItem() {
+        this.router.navigate(['/detail', this.listory.listoryId]);
+    }
 }
