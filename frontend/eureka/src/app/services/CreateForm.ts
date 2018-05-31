e2cda40e32a8daff856ce920bc92c3f5603494f7
Onat Bas <git@onat.me>
2018-05-31 09:29:40 +0300

    (add) Create Listory page added.
           Reasons
    =====================
    1. Creating of listories are needed
    
           Changes
    =====================
    1. Implmented creation.
    1. App routing has a /create page in which users can create new
    listories.
    2. SimpleStore implemented to store session information in the browser.
    3. create.component implemented, which is he baic component for the
    create form.
    4. create.service implemented, which takes over the execution of saving
    data to backend.
    5. Retrieval of category and timeInfo implemented.

---
+++ frontend/eureka/src/app/services/CreateForm.ts
@@ -0,0 +1,12 @@
+export class CreateForm
+{
+    name:string;
+    description:string;
+    image:string;
+    category:string;
+    timeInfo:{
+        id:string;
+        value_1:string;
+        value_2:string;
+    }
+}
